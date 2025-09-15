c1d5e5e56d96108393d087d02e11c26ff3ce0254
Dejan Bjeloglav <dejanbj@hotmail.com>
2025-09-15 13:13:11 +0200

    Add validations to conversion service

--- lib/currency_converter/conversion_service.rb
+++ lib/currency_converter/conversion_service.rb
@@ -2,6 +2,7 @@
 
 module CurrencyConverter
   class ConversionService
+
     ADAPTER = CurrencyConverter::Adapters::ExchangeRateApiAdapter
 
     def initialize(from_currency:, to_currency:, amount:)
@@ -11,9 +12,32 @@ module CurrencyConverter
     end
 
     def call
+      validate_currencies!
+      validate_amount!
+
       rates = ADAPTER.fetch_rates(@from_currency)
       rate_for_target_currency = rates.rate_for(@to_currency)
       @amount.to_f * rate_for_target_currency
+    end
+
+    private
+
+    def validate_currencies!
+      if !supported_currencies.include?(@to_currency) || !supported_currencies.include?(@from_currency)
+        raise InvalidCurrencyError
+      end
+    end
+
+    def validate_amount!
+      begin
+        Float(@amount)
+      rescue
+        raise InvalidAmountError
+      end
+    end
+
+    def supported_currencies
+      @supported_currencies ||= CurrencyConverter::CurrencyConfig.load_config['currencies']
     end
   end
 end
