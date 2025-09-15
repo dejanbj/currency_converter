0a6567f5fe47fc2227a962c252b56567be3fbd02
Dejan Bjeloglav <dejanbj@hotmail.com>
2025-09-15 13:14:10 +0200

    Quotes related cleanup of main file

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -2,12 +2,13 @@
 
 require 'httparty'
 
-require_relative "currency_converter/version"
-require_relative "currency_converter/currency_config"
-require_relative "currency_converter/adapters/base_adapter"
-require_relative "currency_converter/adapters/exchange_rate_api_adapter"
-require_relative "currency_converter/models/exchange_rate_info"
-require_relative "currency_converter/conversion_service"
+require_relative 'currency_converter/version'
+require_relative 'currency_converter/currency_config'
+require_relative 'currency_converter/adapters/base_adapter'
+require_relative 'currency_converter/adapters/exchange_rate_api_adapter'
+require_relative 'currency_converter/models/exchange_rate_info'
+require_relative 'currency_converter/conversion_service'
+require_relative 'currency_converter/exceptions'
 
 module CurrencyConverter
   class Error < StandardError; end
