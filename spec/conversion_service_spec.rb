fdb5f34e017db401ab852419e8bc12a2cccff55c
Dejan Bjeloglav <dejanbj@hotmail.com>
2025-09-15 13:13:50 +0200

    Improve specs, add spec for amount validation

--- spec/conversion_service_spec.rb
+++ spec/conversion_service_spec.rb
@@ -8,16 +8,17 @@ RSpec.describe CurrencyConverter::ConversionService do
   let(:amount) { '100' }
 
   describe '#call' do
-    let(:adapter_double) { class_double('::CurrencyConverter::Adapters::ExchangeRateApiAdapter') }
     let(:adapter_return_value) do
-      CurrencyConverter::Models::ExchangeRateInfo.new(currency: from_currency, rates: {
-        'EUR' => 5,
-        'JPY' => 10
-      })
+      CurrencyConverter::Models::ExchangeRateInfo.new(currency: from_currency, rates:
+        {
+          'EUR' => 5,
+          'JPY' => 10
+        }
+      )
     end
 
     before do
-      allow(adapter_double).to receive(:fetch_rates).and_return(adapter_return_value)
+      allow(CurrencyConverter::Adapters::ExchangeRateApiAdapter).to receive(:fetch_rates).and_return(adapter_return_value)
     end
 
     it 'returns value of converted amount' do
@@ -27,10 +28,26 @@ RSpec.describe CurrencyConverter::ConversionService do
 
     context 'when from_currency is not valid' do
       let(:from_currency) { 'invalid' }
+
+      it 'raises error' do
+        expect { subject.call }.to raise_error(CurrencyConverter::InvalidCurrencyError)
+      end
     end
 
     context 'when to_currency is not valid' do
       let(:to_currency) { 'invalid' }
+
+      it 'raises error' do
+        expect { subject.call }.to raise_error(CurrencyConverter::InvalidCurrencyError)
+      end
+    end
+
+    context 'when amount is not valid' do
+      let(:amount) { 'invalid' }
+
+      it 'raises error' do
+        expect { subject.call }.to raise_error(CurrencyConverter::InvalidAmountError)
+      end
     end
   end
 end
