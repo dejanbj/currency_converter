28811ee8b2036c0a3a7c3d76bffbd0f6cf2783cd
Dejan Bjeloglav <dejanbj@hotmail.com>
2025-09-15 12:12:08 +0200

    Basic console logic

--- lib/currency_converter/conversion_service.rb
+++ lib/currency_converter/conversion_service.rb
@@ -4,7 +4,7 @@ module CurrencyConverter
   class ConversionService
     ADAPTER = CurrencyConverter::Adapters::ExchangeRateApiAdapter
 
-    def initialize(from_currency, to_currency, amount)
+    def initialize(from_currency:, to_currency:, amount:)
       @from_currency = from_currency
       @to_currency = to_currency
       @amount = amount
@@ -13,7 +13,7 @@ module CurrencyConverter
     def call
       rates = ADAPTER.fetch_rates(@from_currency)
       rate_for_target_currency = rates.rate_for(@to_currency)
-      @amount * rate_for_target_currency
+      @amount.to_f * rate_for_target_currency
     end
   end
 end
